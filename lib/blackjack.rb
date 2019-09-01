654845846e0354ca04a72cf29e5daa7dbb1487ac
Sarah Wise <skjwise@hotmail.com>
2019-09-01 17:22:44 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,7 +34,13 @@ end
 def hit?(card_total)
   prompt_user
   user_input = get_user_input
-  
+  if user_input == "h"
+    card_total += deal_card
+    elsif user_input == "s"
+    card_total
+  else
+    invalid_command
+  end
 end
 
 def invalid_command
